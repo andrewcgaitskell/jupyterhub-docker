1e615caa3619828f6ea0c855da074301df2751ad
andrewcgaitskell <andrew@gaitskell.com>
2022-05-03 12:46:11 +0100

    Update jupyterhub_config.py

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -55,3 +55,21 @@ c.Authenticator.admin_users = {'jupyterhub'}
 #  If empty, does not perform any additional restriction.
 c.Authenticator.allowed_users = {'jupyterhub'}
 
+##
+
+## Docker spawner
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
+c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
+# user data persistence
+# see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
+#notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR')
+#c.DockerSpawner.notebook_dir = notebook_dir
+#c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+# Other stuff
+c.Spawner.cpu_limit = 1
+c.Spawner.mem_limit = '10G'
